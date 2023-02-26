ad225d7c9cb090067bdeeea8b717e7b81e1b1cce
duyntc <duyntc@vng.com.vn>
2023-02-26 12:54:32 +0700

    add ws project

---
+++ spring-ws/src/test/java/com/dng/project/SpringWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dng.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
